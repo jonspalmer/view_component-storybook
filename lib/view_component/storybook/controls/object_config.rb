dac3e351ffbf808d185f08335efd7aba60b5d759
Nicolò Rebughini <nicolo.rebughini@gmail.com>
2021-07-06 09:13:37 +0200

    Add support for Arrays of Objects

--- lib/view_component/storybook/controls/object_config.rb
+++ lib/view_component/storybook/controls/object_config.rb
@@ -10,7 +10,12 @@ module ViewComponent
 
         def value_from_param(param)
           if param.is_a?(String)
-            JSON.parse(param).deep_symbolize_keys
+            parsed_json = JSON.parse(param)
+            if parsed_json.is_a?(Array)
+              parsed_json.map(&:deep_symbolize_keys)
+            else
+              parsed_json.deep_symbolize_keys
+            end
           else
             super(param)
           end
