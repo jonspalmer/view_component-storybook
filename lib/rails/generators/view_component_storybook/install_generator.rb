235893e48c611b8774ae08013ec46dbcc07381ca
William Kennedy <williamkennedyjnr@gmail.com>
2021-07-26 18:56:21 +0100

    initial commit on generator

---
+++ lib/rails/generators/view_component_storybook/install_generator.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require "rails/generators/abstract_generator"
+
+module ViewComponentStorybook
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+
+      source_root File.expand_path("../templates", __FILE__)
+
+      desc "Installs storybook"
+
+      def install
+        insert_into_file '.gitignore', '*/*.stories.json'
+        run_yarn
+        generate_storybook_files
+      end
+
+
+      private
+
+      def run_yarn
+        run 'yarn add @storybook/server @storybook/addon-controls --dev'
+      end
+
+      def generate_storybook_files
+        template 'main.js.tt', '.storybook/main.js'
+        template 'preview.js.tt', '.storybook/preview.js'
+      end
+    end
+  end
+end
