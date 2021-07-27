057362e1cb69f579f30cba49281190dc62981937
William Kennedy <williamkennedyjnr@gmail.com>
2021-07-27 09:20:47 +0100

    removed template line

--- lib/rails/generators/view_component_storybook/install_generator.rb
+++ lib/rails/generators/view_component_storybook/install_generator.rb
@@ -4,8 +4,6 @@ module ViewComponentStorybook
   module Generators
     class InstallGenerator < Rails::Generators::Base
 
-      source_root File.expand_path("../templates", __FILE__)
-
       desc "Installs storybook"
 
       def install
